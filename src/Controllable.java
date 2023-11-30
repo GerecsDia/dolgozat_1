f80bab301c812d04e36625519c33b9b377617f02
Gerecs Diána <gerecsdiana@gmail.com>
2023-11-30 09:45:48 +0100

    hiányzó adatok pótlása

--- src/Controllable.java
+++ src/Controllable.java
@@ -1,3 +1,13 @@
+/*
+* File: dolgozat_1
+* Author: Gerecs Diána
+* Copyright: 2023, Gerecs Diána
+* Group: Szoft II/1/E
+* Date: 2023-11-30
+* Github: https://github.com/GerecsDia/
+* Licenc: GNU GPL
+*/
+
 public interface Controllable {
     void start();
 
